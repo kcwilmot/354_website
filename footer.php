98472ff41e1925b6c20d0ebcb3168d00c6e97204
Kelly Wilmot <kellywilmot@u.boisestate.edu>
2020-09-29 00:46:53 -0600

    footer styling

--- footer.php
+++ footer.php
@@ -1,20 +1,20 @@
 <div>
-    <div id="footer-left">
+    <span id="footer-left">
         <a href="contact.php"> Contact Info </a>
         <li class="footer-socials">
             <a href="https://www.facebook.com/biteycatcustoms/"> Facebook </a>
-</li>
-<li class="footer-socials">
-    <a href="https://www.instagram.com/biteycatcustoms/?hl=en"> Instagram </a>
-</li>
-<li class="footer-socials">
-    <a href="https://www.etsy.com/shop/BiteyCatCustoms"> Etsy </a>
-</li>
-    </div>
-    <div id="footer-right">
+        </li>
+        <li class="footer-socials">
+            <a href="https://www.instagram.com/biteycatcustoms/?hl=en"> Instagram </a>
+        </li>
+        <li class="footer-socials">
+            <a href="https://www.etsy.com/shop/BiteyCatCustoms"> Etsy </a>
+        </li>
+    </span>
+    <span id="footer-right">
         <a href="orderhistory.php"> Order History </a>
         <a href="trackpkg.php"> Track Package </a>
         <a href="myaccount.php"> My Account </a>
-    </div>
+    </span>
 
 </div>
