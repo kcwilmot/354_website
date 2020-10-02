08daa6b0c7835c2e06ca8e0cc9eef465b12c0e8b
Kelly Wilmot <kellywilmot@u.boisestate.edu>
2020-10-02 02:10:44 -0600

    header

--- header.php
+++ header.php
@@ -1,17 +1,28 @@
 <html>
     <div id="header-nav">
         <div>
-            Bitey Cat Customs
-            <img src="img/logo.png" alt="Bitey Cat Customs">
+            Bitey Cat Customs<br>
+            <img id="logo" src="img/logo.png" alt="Bitey Cat Customs">
         </div>
-        <li class="header-links">
-            <a href="signin.php"> Sign In </a>
-</li>
-<li class="header-links">
-<a href="checkout.php"> Checkout </a>
-<li class="header-links">
-            <a href="faq.php"> FAQ </a>
-</li>
+        <div>
+            <li class="header-links">
+                <a href="signin.php"> Sign In </a>
+            </li>
+            <li class="header-links">
+                <a href="checkout.php"> Checkout </a>
+            <li class="header-links">
+                <a href="faq.php"> FAQ </a>
+            </li>
+        </div>
+        <form>
+            <label> "Search"</label>
+            <input type="text" id="search-form">
+        </form>
+        <a href="shop/earrings.php" class="header-shop-list">Earrings</a>
+        <a href="shop/customitem.php" class="header-shop-list">Custom Item</a>
+        <a href="about.php" class="header-links">About</a>
+    </div>
+    <hr>
 </html>
 <?php ?>
 
