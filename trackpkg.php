922ddcbfcd342b6c19037b2469faf5e0a0bc3ec3
Kelly Wilmot <kellywilmot@u.boisestate.edu>
2020-10-03 14:55:31 -0600

    styling track pkg

--- trackpkg.php
+++ trackpkg.php
@@ -2,6 +2,9 @@
 <?php
   include_once("header.php");
 ?>
+<head>
+  <link rel="stylsheet" href="style.css">
+</head>
 <h1 id="track-pkg-header">Track Package</h1>
 <div id="tracker-box">
   [INSERT EMBEDED LINK TO SHIPPING COMAPNIES]
