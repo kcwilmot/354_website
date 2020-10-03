59d300026c3c4c4351e9c868f074b739424270b9
Kelly Wilmot <kellywilmot@u.boisestate.edu>
2020-10-02 18:06:26 -0600

    admin item forms

--- shop/admin.php
+++ shop/admin.php
@@ -1,3 +1,23 @@
 <?php
   include_once("header.php");
+?>
+<body> 
+  <div>Inventory</div>
+  <div>Add Item
+    <form id="add-item-form">
+      <label for="item-name">Item Name</label>
+      <input type="text" name="item-name"><br>
+      <label for="item-price">Price</label>
+      <input type="number" name="item-price"><br>
+      <label for="item-image">Image</label>
+      <input type="number" name="item-image"><br>
+      <label for="item-category">Category</label>
+      <input type="number" name="item-category"><br>
+      <label for="submit-new-item">Submit</label>
+      <input type="button" name="submit-new-item"><br>
+</form>
+  </div>
+
+
+<?php
   include_once("footer.php");
