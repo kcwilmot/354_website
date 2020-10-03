5b8087b9e37a374a0fc2bba208d7d65cc808f290
Kelly Wilmot <kellywilmot@u.boisestate.edu>
2020-10-03 14:53:04 -0600

    styling and fix header/footer in custom item pg

--- trackpkg.php
+++ trackpkg.php
@@ -1,4 +1,11 @@
 <!--header, track pkg word, box for link to ups, footer-->
 <?php
   include_once("header.php");
-  include_once("footer.php");
+?>
+<h1 id="track-pkg-header">Track Package</h1>
+<div id="tracker-box">
+  [INSERT EMBEDED LINK TO SHIPPING COMAPNIES]
+</div>
+
+
+ <?php include_once("footer.php");
