da9f0404f8e64ca463158f4c96166f01ba8d9df0
Kelly Wilmot <kellywilmot@u.boisestate.edu>
2020-10-04 15:18:24 -0600

    stubbign out pages

--- about.php
+++ about.php
@@ -1,4 +1,30 @@
 <!--header, pic, about words, name, bio, paragraph, other stuff links, footer-->
 <?php
   include_once("header.php");
+?>
+<div id="about-image-container">
+<img id="about-img" src="img/logo.png">
+</div>
+<div id="about-info-container">
+  <h2>About Us</h2>
+  Name: Colette And Kale<br>
+  Bio:
+  <div id="about-bio-container">
+    Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. 
+    De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo 
+    vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. 
+    Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus 
+    resi dentevil vultus comedat cerebella viventium. Qui animated corpse, 
+    cricket bat max brucks terribilem incessu zomby. The voodoo sacerdos flesh 
+    eater, suscitat mortuos comedere carnem virus. Zonbi tattered for solum 
+    oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. 
+    Sicut malus putrid voodoo horror. Nigh tofth eliv ingdead.
+  </div>
+  <h1>Other Links</h1>
+<li><a href="https://www.youtube.com/watch?v=i-p9lWIhcLQ&list=PL1tiwbzkOjQz7D0l_eLJGAISVtcL7oRu_">
+Critical Role</a>
+</li>
+<li><a href="https://pixabay.com/images/search/cat/">Cat Pics</a>
+</li>
+<?php
   include_once("footer.php");
--- myaccount.php
+++ myaccount.php
@@ -1,4 +1,21 @@
 <!--header, shows name and email, change password link, address info, footer-->
 <?php
   include_once("header.php");
+?>
+
+<h1 id="my-account-header">My Account</h1>
+Name:  username<br>
+Email: email@emailservice.com
+
+<a href="myaccount.php">Change Password?</a><br>
+<div>
+  Shipping Address:
+  <div id="account-shipping-address">
+    555 A Street Adress
+    Apt 666
+    City, ST 12345
+  </div>
+</div>
+
+<?php
   include_once("footer.php");
