3608506c99ca807b56ba3cc3dfda14fd06ebd332
Kelly Wilmot <kellywilmot@u.boisestate.edu>
2020-11-03 00:18:29 -0700

    working on login handler

--- login_handler.php
+++ login_handler.php
@@ -10,10 +10,12 @@
   $_SESSION['success'] = array();
   $_SESSION['fail'] = array();
 
+  $logger->LogDebug("Entered login_handler");
   //Get user's enter creds, validate, redirect to home if works
   if ($dao->userExists($_POST['email'], $_POST['password'])) {
     $_SESSION['authenticated'] = true;
     header("Location: https://polar-plains-93513.herokuapp.com/index.php");
+    exit();
   } else {
     //redirect back to login form with an error
     $logger->LogError("User entered invalid username or password");
