56463e05ec522aaf021eb03c7c09716b6b237251
Kelly Wilmot <kellywilmot@u.boisestate.edu>
2020-11-08 22:52:40 -0700

    login

--- login_handler.php
+++ login_handler.php
@@ -12,6 +12,7 @@
   $_SESSION['username'] = $user->email;
   $_SESSION['success'] = array();
   $_SESSION['fail'] = array();
+  $_SESSION['authenticated'] = false;
 
   $logger->LogDebug("Entered login_handler");
   //print_r($_POST);
@@ -24,32 +25,16 @@
   $logger->LogDebug("Ret val is: " . gettype($ret_val));
 
 
-  if ($ret_val > "0") {
+  if ($ret_val > 0) {
     $logger->LogDebug("User authenticated: [{$user}]");
     $_SESSION['authenticated'] = true;
     //print_r($_SESSION['authenticated']); 
-    header("Location: index.php");
+    header("Location: https://polar-plains-93513.herokuapp.com/index.php");
     exit();
   } else {
         $logger->LogDebug("Failed to get user: [{$user}]");
-        $_SESSION['authenticated'] = false;
+        //$_SESSION['authenticated'] = false;
         header("Location: https://polar-plains-93513.herokuapp.com/login.php");
     exit();
   }
-  
-  /*
-  else if (!$dao->user_Exists($user)) {
-      //redirect back to login form with an error
-      $logger->LogError("User does not exist: $user");
-      $_SESSION['fail'][] = "Invalid Username or Password";
-      header("Location: https://polar-plains-93513.herokuapp.com/login.php");
-      exit();
-  } else {
-      $logger->LogError("Invalid Credentials: [{$user}]");
-      header("Location: https://polar-plains-93513.herokuapp.com/login.php");
-    exit();
-  }
-  */
-  //header("Location: https://polar-plains-93513.herokuapp.com/login.php");
-  //    exit();
   
