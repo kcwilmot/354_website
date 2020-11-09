2e4f265dfec0634dab5ca59920dfc4a315669099
Kelly Wilmot <kellywilmot@u.boisestate.edu>
2020-11-09 01:12:19 -0700

    create user

--- signup.php
+++ signup.php
@@ -12,21 +12,7 @@
     <form method="post" action="create_user_handler.php">
       <div>Email: <input type="text" name="email"/></div>
       <div>Password: <input type="password" name="password"/></div>
-      <div><input type="submit" value="Login"></div>
-    </form>
-  </body>
-</html><html>
-  <body>
-    <h1>Login</h1>
-    <?php
-      if (isset($_SESSION['authenticated']) && !$_SESSION['authenticated']) {
-        echo "<div class='bad'>Invalid Username or password</div>";
-      }
-    ?>
-    <form method="post" action="login_handler.php">
-      <div>Email: <input type="text" name="email"/></div>
-      <div>Password: <input type="password" name="password"/></div>
-      <div><input type="submit" value="Login"></div>
+      <div><input type="submit" value="Create User"></div>
     </form>
   </body>
 </html>
