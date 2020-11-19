787924fd3c5002cf3afbf83548d0a8b2c09075e5
Kelly Wilmot <kellywilmot@u.boisestate.edu>
2020-11-18 17:14:07 -0700

    making myaccount page dynamic - debugging

--- myaccount.php
+++ myaccount.php
@@ -1,21 +1,22 @@
 <!--header, shows name and email, change password link, address info, footer-->
 <?php
-  session_start();
+  // session_start();
   include_once("header.php");
+  $dao = new Dao();
+  $logger = new KLogger ("log.txt" , KLogger::DEBUG);
+  $logger->LogDebug("On MyAccount page for user [{$_SESSION['user']}].");
 
   //Check if authenticated
   if ((isset($_SESSION['authenticated']) && !$_SESSION['authenticated']) || !isset($_SESSION['authenticated'])) {
     header("Location: login.php");
   }
 
-  $dao = new Dao();
-  $logger = new KLogger ("log.txt" , KLogger::DEBUG);
-  $logger->LogDebug("On MyAccount page for user [{$_SESSION['user']}].");
+  
   $userInfo = $dao->get_userInfo($_SESSION['user']);
   $logger->LogDebug("User info return: " . print_r($userInfo,1));
   //$logger->LogDebug("Session: " . print_r($_SESSION,1));
 
-  echo print_r($userInfo,1);
+  // echo print_r($userInfo,1);
 ?>
 
 <h1 id="my-account-header">My Account</h1>
