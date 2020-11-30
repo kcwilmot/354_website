e3ca086c98fb791a1aaa3fc2e5ade648ede8df30
Kelly Wilmot <kellywilmot@u.boisestate.edu>
2020-11-30 14:21:28 -0700

    jquery

--- scripts.js
+++ scripts.js
@@ -5,12 +5,14 @@ $(function(){
     })
 })
 
-$("#new_user_tooltip").on({
-    "click": function() {
-        $(this).tooltip({ items: "#tt", content: "Requires a valid email. Passwords must be at least 3 characters in length."});
-        $(this).tooltip("open");
-    },
-    "mouseout": function() {      
-        $(this).tooltip("disable");   
-    }
+$(function(){
+    $("#new_user_tooltip").on({
+        "click": function() {
+            $(this).tooltip({ items: "#tt", content: "Requires a valid email. Passwords must be at least 3 characters in length."});
+            $(this).tooltip("open");
+        },
+        "mouseout": function() {      
+            $(this).tooltip("disable");   
+        }
+    });
 });
